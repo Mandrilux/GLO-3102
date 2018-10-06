4656bb3cacfc2f1e135245ba53f47c90c0d78c87
heraud_b <baptiste.heraud@epitech.eu>
2018-10-06 10:01:26 -0400

    start labo4

---
+++ labo4/js/main.js
@@ -0,0 +1,24 @@
+
+
+window.onload = function (e)
+{
+   /* let ToastId = 0;
+
+    document.getElementById("toastButton").onclick = function(){
+        let title = document.getElementsByName("title")[0].value;
+        let text = document.getElementsByName("text")[0].value;
+        let type = document.getElementsByName("type");
+        for (var i = 0, length = type.length; i < length; i++)
+        {
+            if (type[i].checked)
+            {
+                type = type[i].value;
+                break;
+            }
+        }
+        let toast = new Toast(title, text, type);
+        toast.showInContainer(ToastId);
+        ToastId++;
+    }*/
+
+}
---
+++ labo4/js/toast.js
@@ -0,0 +1,24 @@
+export class Toast {
+    constructor(name, message, etype)
+    {
+        this.name = name;
+        this.message = message;
+        this.etype = etype;
+    }
+
+    showInContainer(id)
+    {
+        let container = document.getElementById("container");
+        container.innerHTML = container.innerHTML + "<div id='" + id +"' class='toast " +this.etype+"'>" +
+            "<p class='titre'>" + this.name +"</p>" +
+            "<p class='message'>" +this.message + "</p> " +
+            "</div>";
+        this.deletein(id);
+    }
+
+    deletein(id){
+        setTimeout(function () {
+            document.getElementById(id).outerHTML = "";
+        }, 3000);
+    }
+}
