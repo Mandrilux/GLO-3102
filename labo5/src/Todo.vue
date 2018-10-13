801bc3e9a5d8af5a35f1a41b86df2e0662c0beda
heraud_b <baptiste.heraud@epitech.eu>
2018-10-13 18:30:19 -0400

    Create component

---
+++ labo5/src/Todo.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="todo">
+        <p> ToDolist</p>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'Todo'
+    }
+</script>
--- labo5/src/main.js
+++ labo5/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Todo from './Todo.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+    render: h => h(App)
 }).$mount('#app')
+
+new Vue({
+    render: h => h(Todo)
+}).$mount('#todo')
