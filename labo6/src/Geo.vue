ca04d65f308ecbc98efb1fe28685b2f304db6601
heraud_b <baptiste.heraud@epitech.eu>
2018-11-01 20:20:28 -0400

    Get info about localisation

---
+++ labo6/src/Geo.vue
@@ -0,0 +1,34 @@
+<template>
+    <section id="wrapp-geolocation">
+        <div @click="edit">FOO</div>
+    </section>
+</template>
+
+
+<script>
+    export default {
+        name: 'Geoloc'
+    }
+
+    //import { TodoList } from './components/todo.js';
+
+
+
+
+
+</script>
+
+
+<style>
+
+    #wrapp-geolocation
+    {
+        margin-top: 70px;
+    }
+
+
+</style>
+
+
+
+
--- labo6/src/main.js
+++ labo6/src/main.js
@@ -1,17 +1,43 @@
 import Vue from 'vue'
-import Todo from './Todo.vue'
+import Geo from './Geo.vue'
 import Footer from './Footer.vue'
 import Header from './Header.vue'
-
-Vue.config.productionTip = false
 
 new Vue({
     render: h => h(Header)
 }).$mount('#header')
 
 new Vue({
-    render: h => h(Todo)
-}).$mount('#todo')
+    el: '#geoloc',
+    data: {
+        lat: '',
+        lon: '',
+        email: '',
+    },
+    render: h => h(Geo),
+    methods: {
+        geolocation() {
+            navigator.geolocation.getCurrentPosition(this.buildUrl, this.geoError);
+        },
+        buildUrl(position) {
+            this.lat = position.coords.latitude;
+            this.lon = position.coords.longitude;
+            console.log(this.lat);
+            console.log(this.lon);
+        },
+        geoError(error) {
+            alert(error);
+        },
+        edit () {
+        console.log("ok")
+        }
+    },
+    beforeMount() {
+        this.geolocation();
+    },
+
+})
+
 
 new Vue({
     render: h => h(Footer)
