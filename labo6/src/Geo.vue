71f275435718cd13cb72990d313c56dcd67001e0
heraud_b <baptiste.heraud@epitech.eu>
2018-11-04 16:48:49 -0500

    Fix

--- labo6/src/Geo.vue
+++ labo6/src/Geo.vue
@@ -1,6 +1,6 @@
 <template>
     <section id="wrapp-geolocation">
-        <input v-model="city" id="city" type="text" name="city" placeholder="Ville...">
+        <input v-model="city" id="city" type="text" name="city" placeholder="Saisir une Ville...">
         <input id="submitButton" type="submit" value="Valider" v-on:click="save(city)">
     </section>
 </template>
@@ -11,13 +11,29 @@
         methods: {
             save: function(city){
                 console.log(city);
+                this.geolocation()
+            },
+            geolocation() {
+                navigator.geolocation.getCurrentPosition(this.buildUrl, this.geoError);
+            },
+            buildUrl(position) {
+                this.lat = position.coords.latitude;
+                this.lon = position.coords.longitude;
+            },
+            geoError(error) {
+                alert(error);
             }
         },
         data() {
             return {
                 city: '',
+                lat: '',
+                lon: ''
             };
-        }
+        },
+        beforeMount() {
+            this.geolocation();
+        },
     }
 
     //import { TodoList } from './components/todo.js';
--- labo6/src/main.js
+++ labo6/src/main.js
@@ -12,38 +12,6 @@ new Vue({
     render: h => h(Geo)
 }).$mount('#geoloc')
 
-/*new Vue({
-    el: '#geoloc',
-    data: {
-        lat: '',
-        lon: '',
-        city: ''
-    },
-    render: h => h(Geo),
-    methods: {
-        geolocation() {
-            navigator.geolocation.getCurrentPosition(this.buildUrl, this.geoError);
-        },
-        buildUrl(position) {
-            this.lat = position.coords.latitude;
-            this.lon = position.coords.longitude;
-            console.log(this.lat);
-            console.log(this.lon);
-        },
-        geoError(error) {
-            alert(error);
-        },
-        edit () {
-        console.log("ok")
-        }
-    },
-    beforeMount() {
-        this.geolocation();
-    },
-
-})*/
-
-
 new Vue({
     render: h => h(Footer)
 }).$mount('#footer')
