cbd3fa8c4914b6700caefd45060a02a304fc58a3
heraud_b <baptiste.heraud@epitech.eu>
2018-11-10 19:49:02 -0500

    add function

--- labo7/index.js
+++ labo7/index.js
@@ -1,6 +1,38 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let cors = require('cors');
+let app = express();
+
+let userId = 0;
+let taskId = 0;
+let tasks = [];
+let corsOptions = {
+    origin : '*',
+    methods : ['GET', 'PUT', 'POST', 'DELETE', 'UPDATE'],
+    credentials: true
+};
+
+app.use(bodyParser());
+app.use(cors(corsOptions));
+
+app.post('/users', function (req, res) {
+     userId++;
+     res.send({id:userId});
+});
+
+app.post('/:id/tasks', function (req, res) {
+    let name = req.body.name;
+    let id = req.params.id;
+    taskId++;
+    tasks.push({userId:id, taskId: taskId, name: name});
+    res.send({id:taskId, name: name});
+});
+
+app.get('/:id', function (req, res) {
+    let id = req.params.id;
+    res.send(id);
+});
 
 
-app.use(bodyParser);
+app.listen(8080, '0.0.0.0')
+console.log('Listening on port 8080....');
