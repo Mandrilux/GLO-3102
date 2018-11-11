2c75f37c826320dfcf04f20182bd1c93e1743217
heraud_b <baptiste.heraud@epitech.eu>
2018-11-11 11:31:00 -0500

    Module

--- labo7/index.js
+++ labo7/index.js
@@ -2,12 +2,11 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let cors = require('cors');
 let app = express();
-const server = require('./server');
+
+const server = require('./modules/server');
+const tasksManager = require('./modules/tasks');
 
 
-let userId = 0;
-let taskId = 0;
-let tasks = [];
 let corsOptions = {
     origin : '*',
     methods : ['GET', 'PUT', 'POST', 'DELETE', 'UPDATE'],
@@ -17,57 +16,11 @@ let corsOptions = {
 app.use(bodyParser());
 app.use(cors(corsOptions));
 
-app.post('/users', function (req, res) {
-     userId++;
-     res.send({id:userId});
-});
 
-app.post('/:id/tasks', function (req, res) {
-    let name = req.body.name;
-    let id = req.params.id;
-    taskId++;
-    tasks.push({userId:id, taskId: taskId, name: name});
-    res.send({id:taskId, name: name});
-});
-
-app.get('/:id', function (req, res) {
-    let id = req.params.id;
-    res.send(id);
-});
-
-//on delete la tache
-app.delete('/:id/tasks/:task', function (req, res) {
-    console.log(tasks);
-    let flag = -1;
-    let id = req.params.task;
-    let userId = req.params.id;
-
-    for (let i = 0; i < tasks.length; i++){
-        if (id == tasks[i].taskId && userId == tasks[i].userId) {
-            flag = i;
-        }
-    }
-    if (flag != -1){
-        tasks.splice(flag, 1);
-        console.log(tasks)
-    }
-    res.send({});
-});
-
-// on get les taches
-
-app.get('/:id/tasks', function (req, res) {
-    let userId = req.params.id;
-    taskstmp = [];
-    for (let i = 0; i < tasks.length; i++){
-        if (userId == tasks[i].userId) {
-            taskstmp.push(tasks[i]);
-        }
-    }
-    console.log(tasks);
-    res.send(taskstmp);
-});
-
-
+app.post('/users', tasksManager.getUser);
+app.get('/:id', tasksManager.getId);
+app.get('/:id/tasks', tasksManager.getTasksByID);
+app.post('/:id/tasks', tasksManager.addTasks);
+app.delete('/:id/tasks/:task', tasksManager.deleteTasks);
 
 server.start(app);
---
+++ labo7/modules/tasks.js
@@ -0,0 +1,51 @@
+let userId = 0;
+let taskId = 0;
+let tasks = [];
+
+exports.getUser =  function (req, res) {
+    userId++;
+    res.send({id:userId});
+};
+
+exports.getId =  function (req, res) {
+    let id = req.params.id;
+    res.send(id);
+};
+
+exports.getTasksByID = function (req, res) {
+    let userId = req.params.id;
+    taskstmp = [];
+    for (let i = 0; i < tasks.length; i++){
+        if (userId == tasks[i].userId) {
+            taskstmp.push(tasks[i]);
+        }
+    }
+    res.send(taskstmp);
+};
+
+exports.addTasks = function (req, res) {
+    let name = req.body.name;
+    let id = req.params.id;
+    taskId++;
+    tasks.push({userId:id, taskId: taskId, name: name});
+    res.send({id:taskId, name: name});
+};
+
+
+exports.deleteTasks = function (req, res) {
+    let flag = -1;
+    let id = req.params.task;
+
+    let userId = req.params.id;
+
+    for (let i = 0; i < tasks.length; i++){
+        if (id == tasks[i].taskId && userId == tasks[i].userId) {
+            flag = i;
+        }
+    }
+    if (flag != -1){
+        tasks.splice(flag, 1);
+        console.log(tasks)
+    }
+    res.send({});
+};
