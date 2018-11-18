2dc12b3ad21bf422eaa6c33470ce733f9371f18b
baptiste heraud <baptiste.heraud@epitech.eu>
2018-11-18 18:03:24 -0500

    Fix user login

--- labo8/app.js
+++ labo8/app.js
@@ -4,6 +4,7 @@ let cors = require('cors');
 let cookieParser = require('cookie-parser');
 let app = express();
 let userId = 0;
+var users = []
 
 const server = require('./modules/server');
 
@@ -12,6 +13,7 @@ let corsOptions = {
     methods : ['GET', 'PUT', 'POST', 'DELETE', 'UPDATE'],
     credentials: true
 };
+
 app.use(bodyParser());
 app.use(cookieParser());
 app.use(cors(corsOptions));
@@ -21,17 +23,34 @@ app.get('/login', function(req, res){
     res.sendfile("client/login.html");
 });
 
+app.post('/users', function(req, res){
+  let user = req.body.user
+  let password = req.body.password
+  let userStorage = { username : user, password: password, id : userId }
+  users.push(userStorage);
+  userId++;
+  res.status(200).send({user:users});
+});
+
 app.post('/login', function(req, res){
     let user = req.body.user
     let password = req.body.password
-
-    if (user === "baptiste" && password === "test"){
-      userId++;
-      res.status(200).send({id:userId});
+    let flag = 0;
+    let idrtn;
+    for(var i = 0; i < users.length; i++) {
+      var obj = users[i];
+      if (user === obj.username && password === obj.password){
+        flag = 1;
+        idrtn = obj.id;
+      }
+  }
+    if (flag){
+      console.log("OK");
+      res.status(200).send({id:idrtn});
     }
     else{
       console.log("KO");
-      res.status(401).render();
+      res.status(401).send({});
     }
 });
 
