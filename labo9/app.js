66e2aaf4fbea3b1df99595bdbbec617ec0a028d3
baptiste heraud <baptiste.heraud@epitech.eu>
2018-11-25 11:13:12 -0500

    create project 9

---
+++ labo9/app.js
@@ -0,0 +1,20 @@
+const serverDistrib = require('./modules/server');
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+let cors = require('cors');
+let corsOptions = {
+    origin : '*',
+    methods : ['GET', 'PUT', 'POST', 'DELETE', 'UPDATE'],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+
+app.get('/', function(req, res)){
+  let id = req.params.id;
+  res.sendfile("client/index.html");
+}
+
+serverDistrib.start(app);
